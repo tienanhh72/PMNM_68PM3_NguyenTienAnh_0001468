32a76f43697b52aecfd5fded470e98d27164b78e
Nguyen Tien Anh <tienanhhihi72@gmail.com>
2026-05-14 14:33:35 +0700

    Thực hành 14/5

---
+++ app/controllers/sinhvien.php
@@ -0,0 +1,9 @@
+<?php
+class sinhVien {
+    public function index() {
+        echo "Đây là danh sách sinh viên";
+    }
+    public function create() {
+        echo "Đây là trang tạo sinh viên";
+    }
+}
